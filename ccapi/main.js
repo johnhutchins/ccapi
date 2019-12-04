f47071aa116c258e8ce31f7e5097bf04c96de156
John Hutchins <johnwallshutchins@gmail.com>
2019-12-03 21:46:05 -0500

    progress toward dev server with mock data

--- ccapi/main.js
+++ ccapi/main.js
@@ -1,10 +1,11 @@
 const fetch = require("node-fetch")
 
 const BASE_URL = 'https://www.strava.com/api/v3/'
+const TEST_URL = 'localhost:5000/index'
 
 async function getActivity(){
     //TODO grab acitivyId from somewhere
-    fetch(BASE_URL + '98765432123456789')
+    fetch(TEST_URL)
     .then(json => console.log(json))
 }
 
