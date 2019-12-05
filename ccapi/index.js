4619186a952aef419eb80a394069b78abe67bd3e
John Hutchins <johnwallshutchins@gmail.com>
2019-12-05 14:13:13 -0500

    Decode strava polylines to get lat/long #6

--- ccapi/index.js
+++ ccapi/index.js
@@ -1,5 +1,5 @@
-
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
+const polyline = require('google-polyline')
 
 //const BASE_URL = 'https://www.strava.com/api/v3/'
 const TEST_URL = 'http://localhost:5000'
@@ -9,14 +9,25 @@ async function requestFetch(TEST_URL) {
     .then(response => response.json())
 }
 
-(async () => {
-  console.log('The response is: ')
-  const result = await requestFetch(TEST_URL)
-  const smallerResult = {
-    id: result['id'],
-    external_id: result['external_id'],
-  };
-  console.log(smallerResult)
-  return smallerResult
-})();
+async function returnId() {
+    const result = await requestFetch(TEST_URL)
+    const smallerResult = {
+        mapPolyLine: result['map']['polyline']
+    }
+    return smallerResult
+}
+
+async function getMapPolyline(){
+    let res = await returnId()
+    return polyline.decode(res.mapPolyLine)
+}
+
+async function latLongToElevation(){
+    let latLongArray = await getMapPolyline()
+    latLongArray.forEach((pair) =>{
+        console.log(pair)
+    })
+}
+
+latLongToElevation()
 
