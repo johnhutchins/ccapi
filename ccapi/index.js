f375993d5ed394a583d86c4e063a17391496fffa
John Hutchins <johnwallshutchins@gmail.com>
2019-12-05 11:22:22 -0500

    cleanup + ready for consumption

--- ccapi/index.js
+++ ccapi/index.js
@@ -1,44 +1,22 @@
 
 const fetch = require('node-fetch');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 //const BASE_URL = 'https://www.strava.com/api/v3/'
-const TEST_URL = 'http://localhost:5000';
-
-async function getActivity(){
-    fetch(TEST_URL)
-        .then(response => response.json())
-        .then(data => console.log(typeof data))
-}
-getActivity()
-
-//example-using-fetch
-getActivity() 
+const TEST_URL = 'http://localhost:5000'
 
 async function requestFetch(TEST_URL) {
   return fetch(TEST_URL)
-    .then(response => response.json());
-}
-
-function requestAJAX(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      callback(JSON.parse(xhr.responseText));
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.send();
+    .then(response => response.json())
 }
 
 (async () => {
-  console.log('The response is: ');
-  const result = await requestFetch(TEST_URL);
+  console.log('The response is: ')
+  const result = await requestFetch(TEST_URL)
   const smallerResult = {
     id: result['id'],
     external_id: result['external_id'],
   };
-  console.log(smallerResult);
-  return smallerResult;
+  console.log(smallerResult)
+  return smallerResult
 })();
 
