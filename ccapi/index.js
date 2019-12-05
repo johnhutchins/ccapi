fa76e72e65c912ac2678f7aedc22eac905992fa0
Joshua Burke <burke.joshua.james@gmail.com>
2019-12-05 10:49:51 -0500

    These changes show how to use node-fetch, Promises, and await
    This may feel easier to use rather than XMLHttpRequest event based.

--- ccapi/index.js
+++ ccapi/index.js
@@ -1,8 +1,8 @@
-const fetch = require("node-fetch")
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require('node-fetch');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 //const BASE_URL = 'https://www.strava.com/api/v3/'
-const TEST_URL = 'http://localhost:5000'
+const TEST_URL = 'http://localhost:5000';
 
 /* async function getActivity(){
     fetch(TEST_URL)
@@ -10,18 +10,30 @@ const TEST_URL = 'http://localhost:5000'
 }
 
 getActivity() */
- 
-requestAJAX(TEST_URL, (da)=>{
-    console.log(da)
-})
+
+async function requestFetch(TEST_URL) {
+  return fetch(TEST_URL)
+    .then(response => response.json());
+}
 
 function requestAJAX(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback(JSON.parse(xhr.responseText))
-        }
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      callback(JSON.parse(xhr.responseText));
     }
-    xhr.open('GET', url, true)
-    xhr.send()
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
 }
+
+(async () => {
+  console.log('The response is: ');
+  const result = await requestFetch(TEST_URL);
+  const smallerResult = {
+    id: result['id'],
+    external_id: result['external_id'],
+  };
+  console.log(smallerResult);
+  return smallerResult;
+})();
