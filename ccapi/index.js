18d85db74c1cb1095f02399d52e1291aae65a52b
John Hutchins <johnwallshutchins@gmail.com>
2019-12-05 20:01:51 -0500

    progress toward env var setup

--- ccapi/index.js
+++ ccapi/index.js
@@ -1,5 +1,9 @@
 const fetch = require('node-fetch')
 const polyline = require('google-polyline')
+const { API_KEY } = require('../config')
+console.log(API_KEY)
+
+let ELEV_PROFILE_URL = "http://open.mapquestapi.com/elevation/v1/profile"
 
 //const BASE_URL = 'https://www.strava.com/api/v3/'
 const TEST_URL = 'http://localhost:5000'
@@ -9,7 +13,7 @@ async function requestFetch(TEST_URL) {
     .then(response => response.json())
 }
 
-async function returnId() {
+async function getMapPolyline() {
     const result = await requestFetch(TEST_URL)
     const smallerResult = {
         mapPolyLine: result['map']['polyline']
@@ -17,17 +21,38 @@ async function returnId() {
     return smallerResult
 }
 
-async function getMapPolyline(){
-    let res = await returnId()
+async function getLatLong(){
+    const res = await getMapPolyline()
     return polyline.decode(res.mapPolyLine)
 }
 
-async function latLongToElevation(){
-    let latLongArray = await getMapPolyline()
-    latLongArray.forEach((pair) =>{
-        console.log(pair)
-    })
+async function latLongArrBuilder(){
+    const latLongObj = await getLatLong()
+    //need latLongArray to be an actual array
+    let latLongArray = []
+    for(let i=0;i<latLongObj.length;i++){
+        latLongArray.push(latLongObj[i])
+    }
+    return latLongArray
 }
 
-latLongToElevation()
+async function getElevation(){
+    //example
+    //http://open.mapquestapi.com/elevation/v1/profile?key=CbuVY4beH3NvRW5MMm3cctx6YRqOYrw7&shapeFormat=raw&latLngCollection=39.74012,-104.9849,39.7995,-105.7237,39.6404,-106.3736
+    const latLongArr = await latLongArrBuilder()
 
+    let s = '&shapeFormat=json&latLngCollection='
+    s += latLongArr
+
+    ELEV_PROFILE_URL += api_key.api_key
+    ELEV_PROFILE_URL += s
+    let res = encodeURI(ELEV_PROFILE_URL)
+    console.log(res)
+    try{
+        //http://open.mapquestapi.com/elevation/v1/profile?key=CbuVY4beH3NvRW5MMm3cctx6YRqOYrw7&shapeFormat=json&latLngCollection=37.82822,-122.26348
+        //const getElevationProfile = requestFetch(res)
+    } catch(e){
+        console.log(e)
+    }
+}
+getElevation()
