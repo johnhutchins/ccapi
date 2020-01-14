cc9b8841984bbd55064815cf59477ba0cf7d32f4
John Hutchins <johnwallshutchins@gmail.com>
2020-01-14 14:12:00 -0500

    update get

--- ccapi/server.js
+++ ccapi/server.js
@@ -1,3 +1,6 @@
 let express = require('express')
 let app = express()
-app.get('/static', express.static(path.join(__dirname, 'ccapi/'))) 
+//app.get('/static', express.static(path.join(__dirname, 'ccapi/'))) 
+app.get('/index.js', function (req, res) {
+    res.send('index.js')
+  })
