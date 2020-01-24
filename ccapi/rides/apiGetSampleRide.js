6e5307f2b221a6c0704b0cfe2cd1768ef3f118ef
John Hutchins <johnwallshutchins@gmail.com>
2020-01-24 17:44:29 -0500

    remove files, ready for merge

--- ccapi/rides/apiGetSampleRide.js
+++
@@ -1,7 +0,0 @@
-import { DataStore } from //"../../data/data"
-// import { RequestHandler } from "express"
-// import { TourSummary } from "../../model/tourSummary"
-
-export const apiGetTours: RequestHandler = (req, res, next) =>{
-    res.json(DataStore.tours)
-}
--- data/data.js
+++
@@ -1,5 +0,0 @@
-import sampleB from "./samples/sampleB.json"
-
-export class DataStore {
-    static sampleB = sampleB
-}
