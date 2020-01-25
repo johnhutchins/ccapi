5ae1d68721c3ec410acf296888e5c6cc57f93a17
John Hutchins <johnwallshutchins@gmail.com>
2020-01-24 21:21:02 -0500

    get individual ride endpoint

--- api/apiGetIndividualRide.ts
+++ api/apiGetIndividualRide.ts
@@ -5,8 +5,10 @@ import { RideSummary } from "../model/shared/rideSummary"
 export const apiGetIndividualRide: RequestHandler = (req,res,next) =>{
     //id is a string here. which i think is good
     const Id = parseInt(req.params.id)
-    console.log(req.params.id)
-    if(DataStore.rides.find((element) => element.id == Id)){
-        res.json(DataStore.rides.map((item:any) => new RideSummary(item)))
-    }    
+    DataStore.rides.find((element) => {
+        if(element.id === Id){
+            //res.send(element)
+            res.send(new RideSummary(element))
+        } 
+    })
 }
--- build/api/apiGetIndividualRide.js
+++ build/api/apiGetIndividualRide.js
@@ -5,8 +5,10 @@ const rideSummary_1 = require("../model/shared/rideSummary");
 exports.apiGetIndividualRide = (req, res, next) => {
     //id is a string here. which i think is good
     const Id = parseInt(req.params.id);
-    console.log(req.params.id);
-    if (data_1.DataStore.rides.find((element) => element.id == Id)) {
-        res.json(data_1.DataStore.rides.map((item) => new rideSummary_1.RideSummary(item)));
-    }
+    data_1.DataStore.rides.find((element) => {
+        if (element.id === Id) {
+            //res.send(element)
+            res.send(new rideSummary_1.RideSummary(element));
+        }
+    });
 };
