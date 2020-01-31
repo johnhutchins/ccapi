7f86cfd858133632b9e96450580cbf00fec705e3
John Hutchins <johnwallshutchins@gmail.com>
2020-01-30 19:36:23 -0500

    strava oauth

--- api/apiGetStravaAuth.ts
+++
@@ -1,7 +0,0 @@
-//https://www.strava.com/oauth/authorize
-//import { DataStore } from "../data/data"
-import { RequestHandler } from "express"
-
-export const apiGetSegment: RequestHandler = (req,res,next) =>{
-    
-}
---
+++ api/apiGetStravaOauth.ts
@@ -0,0 +1,10 @@
+//https://www.strava.com/oauth/authorize
+//import { DataStore } from "../data/data"
+import { RequestHandler } from "express"
+
+export const apiGetStravaOauth: RequestHandler = (req,res,next) =>{
+    //IF response successful...
+    //THEN code parameter contains the authorization code required to complete the authentication process. 
+
+    
+}
