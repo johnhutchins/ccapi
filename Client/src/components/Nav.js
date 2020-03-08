ddac0924ee7bd6bed455af6952f7dfe3e503e550
Danny Holland <holland.danny@gmail.com>
2020-03-07 18:38:21 -0700

    Added routing to app so it will have different pages.

--- Client/src/components/Nav.js
+++ Client/src/components/Nav.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-function Nav (){
-  return (
-    <footer className="app-footer">
-      <ul>
-        <li>
-            <i class="fa fa-home"></i>
-            Home
-          </li>
-          <li>
-          <i class="fa fa-biking"></i>
-            Bike
-          </li>
-          <li>
-            <i class="fa fa-user"></i>
-            Account
-          </li>
-          <li>
-            <i class="fa fa-map-marker"></i>
-            Find Store
-          </li>
-      </ul>
-    </footer>
-  )
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+	return (
+		<nav className='app-footer'>
+			<ul>
+				<NavLink to='/'>
+					<li>
+						<i class='fa fa-home'></i>
+						Home
+					</li>
+				</NavLink>
+				<NavLink to='/signin'>
+					<li>
+						<i class='fa fa-biking'></i>
+						Bike
+					</li>
+				</NavLink>
+				<NavLink to='/chooseworkout'>
+					<li>
+						<i class='fa fa-user'></i>
+						Account
+					</li>
+				</NavLink>
+				<NavLink to='/workout'>
+					<li>
+						<i class='fa fa-map-marker'></i>
+						Find Store
+					</li>
+				</NavLink>
+			</ul>
+		</nav>
+	);
 }
-export default Nav
+export default Nav;
---
+++ Client/src/pages/ChooseWorkout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChooseWorkout() {
+	return <div>Choose Workout</div>;
+}
+
+export default ChooseWorkout;
