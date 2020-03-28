c74517b6e65ade4f7ee552c166795eeb1ab50b19
John Hutchins <johnwallshutchins@gmail.com>
2020-03-27 20:40:13 -0400

    'stash' strava branch

--- api/apiGetStravaOauth.ts
+++ api/apiGetStravaOauth.ts
@@ -1,10 +1,16 @@
 //https://www.strava.com/oauth/authorize
 //import { DataStore } from "../data/data"
 import { RequestHandler } from "express"
+import { Passport } from "passport"
 
 export const apiGetStravaOauth: RequestHandler = (req,res,next) =>{
+
+
     //IF response successful...
     //THEN code parameter contains the authorization code required to complete the authentication process. 
+    //IE, filter through query string to pull out the access code 
 
-    
+
+    //application must now call the POST https://www.strava.com/oauth/token 
+    //with its client ID and client secret to exchange the authorization code for a refresh token and short-lived access token.
 }
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import { apiGetRides } from "./api/apiGetRides"
 import { apiGetIndividualRide } from "./api/apiGetIndividualRide"
 import { apiGetSegment } from "./api/apiGetSegment"
 import path from "path"
+//import { passport } from "passport-strava-oauth2"
+
 const app = express()
 
 app.use(express.static('public'))
@@ -16,6 +18,16 @@ app.get('/rides', apiGetRides)
 app.get('/rides/:id', apiGetIndividualRide)
 
 app.get('/segment/:id', apiGetSegment)
+
+// app.get('/auth/strava',
+//   passport.authenticate('strava'));
+
+// app.get('/auth/strava/callback', 
+//   passport.authenticate('strava', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//   });
 
 app.listen(process.env.PORT || 8091, ()=>{
     console.log("Server Started")
